f9ed304db2689b1b942e8a743669073dc8ac3944
ghosh128 <ghosh128@umn.edu>
2019-08-21 16:13:36 -0500

    Update data.py

--- SOURCE/IMBALANCED/data.py
+++ SOURCE/IMBALANCED/data.py
@@ -50,8 +50,8 @@ for i in np.unique(O):
     data_weak = np.vstack((data_weak, np.concatenate((X[index[num_strong:],:],Y[index[num_strong:],:],O[index[num_strong:],:]), axis = 1)))
 data_strong = data_strong[1:,:]
 data_weak = data_weak[1:,:]
-np.save(os.path.join(config.NUMPY_DIR[3:], "data_strong_"+str(len(bins)+1)), data_strong)
-np.save(os.path.join(config.NUMPY_DIR[3:], "data_weak_"+str(len(bins)+1)), data_weak)
+#np.save(os.path.join(config.NUMPY_DIR[3:], "data_strong_"+str(len(bins)+1)), data_strong)
+#np.save(os.path.join(config.NUMPY_DIR[3:], "data_weak_"+str(len(bins)+1)), data_weak)
 #%%
 #print("READ MAT DATA")
 #mat_data = {}
@@ -108,14 +108,14 @@ slack_var_lower = x[:N_s]
 slack_var_upper = x[N_s:2*N_s]
 theta = x[2*N_s:2*N_s+num_levels-1]
 
-np.save(os.path.join(config.NUMPY_DIR[3:], "theta_"+str(len(bins)+1)), theta)
+#np.save(os.path.join(config.NUMPY_DIR[3:], "theta_"+str(len(bins)+1)), theta)
 #%%
 print("COMPUTE POSITIVENESS")
 positiveness = np.zeros(num_levels)
 for i in np.unique(O_s):
     positiveness[int(i-1)] = np.mean(Y_s[np.where(O_s == i)[0]])
 
-np.save(os.path.join(config.NUMPY_DIR[3:], "positiveness_"+str(len(bins)+1)), positiveness)
+#np.save(os.path.join(config.NUMPY_DIR[3:], "positiveness_"+str(len(bins)+1)), positiveness)
 #%%
 shutil.rmtree("/".join(config.DATA_DIR.split("/")[:-1]))
 #%%
@@ -126,10 +126,10 @@ print(len(np.where(O==4)[0]))
 print(len(np.where(O==5)[0]))
 print(len(np.where(O==6)[0]))
 #%%
-from scipy import io
-
-strong_data = np.load(os.path.join(config.NUMPY_DIR[3:], "data_strong_6.npy"))
-weak_data = np.load(os.path.join(config.NUMPY_DIR[3:], "data_weak_6.npy"))
-
-scipy.io.savemat(os.path.join(config.NUMPY_DIR[3:],'strong_data.mat'), {'strong_data': strong_data})
-scipy.io.savemat(os.path.join(config.NUMPY_DIR[3:],'weak_data.mat'), {'weak_data': weak_data})
+#from scipy import io
+#
+#strong_data = np.load(os.path.join(config.NUMPY_DIR[3:], "data_strong_6.npy"))
+#weak_data = np.load(os.path.join(config.NUMPY_DIR[3:], "data_weak_6.npy"))
+#
+#scipy.io.savemat(os.path.join(config.NUMPY_DIR[3:],'strong_data.mat'), {'strong_data': strong_data})
+#scipy.io.savemat(os.path.join(config.NUMPY_DIR[3:],'weak_data.mat'), {'weak_data': weak_data})
